7a145b0d4f08b54f8dab428289a02369eef79907
harshilkumar691 <128027207+harshilkumar691@users.noreply.github.com>
2024-05-20 21:33:58 +1000

    Update server.py

--- server.py
+++ server.py
@@ -4,14 +4,59 @@ TCP server for key exchange and authenticated encryption
 import argparse
 from tcpclientserver import TCPServer
 import logging
+import os
+from cryptography.hazmat.primitives.asymmetric import dh
+from cryptography.hazmat.primitives.serialization import Encoding, PublicFormat
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives.hashes import SHA256
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.padding import PKCS7
+from cryptography.hazmat.backends import default_backend
 
 logger = logging.getLogger("DEMO_SERVER")
 
+# Function to perform Diffie-Hellman key exchange and return the shared secret key
+def diffie_hellman_key_exchange(server):
+    parameters = dh.generate_parameters(generator=2, key_size=2048, backend=default_backend())
+    server_private_key = parameters.generate_private_key()
+    server_public_key = server_private_key.public_key()
 
-def server_protocol(server):
+    server_public_key_bytes = server_public_key.public_bytes(Encoding.PEM, PublicFormat.SubjectPublicKeyInfo)
+    server.send(server_public_key_bytes.decode())
+
+    client_public_key_bytes = server.recv().encode()
+    client_public_key = serialization.load_pem_public_key(client_public_key_bytes, backend=default_backend())
+
+    shared_key = server_private_key.exchange(client_public_key)
+    derived_key = HKDF(algorithm=SHA256(), length=32, salt=None, info=b'handshake data', backend=default_backend()).derive(shared_key)
+
+    return derived_key
+
+# Function to encrypt a message using AES
+def encrypt_message(key, message):
+    iv = os.urandom(16)
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+    padder = PKCS7(algorithms.AES.block_size).padder()
+    padded_data = padder.update(message.encode()) + padder.finalize()
+    ciphertext = encryptor.update(padded_data) + encryptor.finalize()
+    return iv + ciphertext
+
+# Function to decrypt a message using AES
+def decrypt_message(key, ciphertext):
+    iv = ciphertext[:16]
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    decryptor = cipher.decryptor()
+    unpadder = PKCS7(algorithms.AES.block_size).unpadder()
+    padded_data = decryptor.update(ciphertext[16:]) + decryptor.finalize()
+    plaintext = unpadder.update(padded_data) + unpadder.finalize()
+    return plaintext.decode()
+
+def server_protocol(server, shared_key):
     '''
     Process the received request and create a response
     :param server: socket used by server
+    :param shared_key: AES key used for encryption/decryption
     :returns: Message to be sent to client
     '''
 
@@ -22,10 +67,12 @@ def server_protocol(server):
         while True:
             # Send prompt to client
             tx_message = "PROMPT:" + prompt
-            server.send(tx_message)
+            encrypted_tx_message = encrypt_message(shared_key, tx_message)
+            server.send(encrypted_tx_message.hex())
 
             # Receive response from client
-            rx_message = server.recv()
+            encrypted_rx_message = bytes.fromhex(server.recv())
+            rx_message = decrypt_message(shared_key, encrypted_rx_message)
             rx_message_fields = rx_message.split(":")
             rx_message_type = rx_message_fields[0]
             rx_message_data = rx_message_fields[1].strip()
@@ -40,16 +87,20 @@ def server_protocol(server):
                 break
             else:
                 # Notify client to enter the details for the empty field
-                server.send("PROMPT:Please enter " + prompt)
+                tx_message = "PROMPT:Please enter " + prompt
+                encrypted_tx_message = encrypt_message(shared_key, tx_message)
+                server.send(encrypted_tx_message.hex())
 
-    # Write details to a file
-    with open("student_details.txt", "w") as file:
-        for key, value in responses.items():
-            file.write(f"{key} {value}\n")
+    # Append details to a file
+    with open("student_details.txt", "a") as file:
+        file.write(f"name: {responses['name:']}\n")
+        file.write(f"id number: {responses['id number:']}\n")
+        file.write(f"unit name: {responses['unit name:']}\n\n")
 
     # Send final message to client
-    tx_message = f"INFO:{responses['name:']} has been added to the unit {responses['unit name:']}."
-    server.send(tx_message)
+    final_message = f"INFO:{responses['name:']} has been added to the unit {responses['unit name:']}."
+    encrypted_final_message = encrypt_message(shared_key, final_message)
+    server.send(encrypted_final_message.hex())
 
     return 0
 
@@ -77,8 +128,11 @@ if __name__ == '__main__':
         # Blocks until client initiates a connection
         server.accept()
 
+        # Perform Diffie-Hellman key exchange
+        shared_key = diffie_hellman_key_exchange(server)
+
         # Process messages
-        server_protocol(server)
+        server_protocol(server, shared_key)
 
         # Close the data socket and wait for more clients to connect
         server.close()
