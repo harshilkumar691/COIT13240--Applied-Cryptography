f6975b75c78f60cc707fb7c55751ad516e97494b
harshilkumar691 <128027207+harshilkumar691@users.noreply.github.com>
2024-05-20 14:50:35 +1000

    Update server.py

--- server.py
+++ server.py
@@ -15,29 +15,40 @@ def server_protocol(server):
     :returns: Message to be sent to client
     '''
 
-    prompts = ["name:", "unit name:"]
-    responses = []
+    prompts = ["name:", "id number:", "unit name:"]
+    responses = {}
 
     for prompt in prompts:
-        # Send prompt to client
-        tx_message = "PROMPT:" + prompt
-        server.send(tx_message)
+        while True:
+            # Send prompt to client
+            tx_message = "PROMPT:" + prompt
+            server.send(tx_message)
 
-        # Receive response from client
-        rx_message = server.recv()
-        rx_message_fields = rx_message.split(":")
-        rx_message_type = rx_message_fields[0]
-        rx_message_data = rx_message_fields[1]
-        logger.debug("Received: %s", rx_message)
+            # Receive response from client
+            rx_message = server.recv()
+            rx_message_fields = rx_message.split(":")
+            rx_message_type = rx_message_fields[0]
+            rx_message_data = rx_message_fields[1].strip()
+            logger.debug("Received: %s", rx_message)
 
-        if rx_message_type != "RESPONSE":
-            logger.error("Received unknown or incorrect message type: %s", rx_message_type)
-            return 1
+            if rx_message_type != "RESPONSE":
+                logger.error("Received unknown or incorrect message type: %s", rx_message_type)
+                return 1
 
-        responses.append(rx_message_data)
+            if rx_message_data:
+                responses[prompt] = rx_message_data
+                break
+            else:
+                # Notify client to enter the details for the empty field
+                server.send("PROMPT:Please enter " + prompt)
+
+    # Write details to a file
+    with open("student_details.txt", "w") as file:
+        for key, value in responses.items():
+            file.write(f"{key} {value}\n")
 
     # Send final message to client
-    tx_message = f"INFO:{responses[0]} has been added to the unit {responses[1]}."
+    tx_message = f"INFO:{responses['name:']} has been added to the unit {responses['unit name:']}."
     server.send(tx_message)
 
     return 0
