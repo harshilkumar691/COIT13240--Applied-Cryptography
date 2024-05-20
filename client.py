674feea08095fbcf05976733744e10845d426ec7
harshilkumar691 <128027207+harshilkumar691@users.noreply.github.com>
2024-05-21 09:47:32 +1000

    Update client.py

--- client.py
+++ client.py
@@ -1,75 +1,92 @@
+'''
+TCP client demo
+'''
 import argparse
-import logging
 from tcpclientserver import TCPClient
-from cryptography.hazmat.primitives import serialization
+import logging
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives.asymmetric import dh
-from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.kdf.hkdf import HKDFExpand
 from cryptography.hazmat.primitives.kdf.concatkdf import ConcatKDFHash
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 import os
 
 logger = logging.getLogger("DEMO_CLIENT")
 
-def client_protocol(client, shared_key):
-    '''
-    Process the received prompt and send response to the server
-    :param client: socket used by client
-    :param shared_key: shared key for encryption/decryption
-    '''
-    while True:
-        # Receive prompt from server
-        encrypted_prompt = client.recv()
-        prompt = decrypt_message(shared_key, encrypted_prompt)
-        if not prompt:
-            break
-        print(prompt, end=" ")
-
-        # Get user input
-        response = input()
-
-        # Send response to server
-        encrypted_response = encrypt_message(shared_key, response)
-        client.send(encrypted_response)
-
-def generate_shared_key(private_key, public_key):
-    '''
-    Generate shared key using ECDH
-    :param private_key: private key
-    :param public_key: public key received from the server
-    :return: shared key
-    '''
-    shared_key = private_key.exchange(ec.ECDH(), public_key)
-    return shared_key
+def derive_key(shared_key):
+    return HKDF(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=None,
+        info=b'handshake data',
+        backend=default_backend()
+    ).derive(shared_key)
 
 def encrypt_message(key, plaintext):
-    '''
-    Encrypt a message using AES in CBC mode
-    :param key: encryption key
-    :param plaintext: message to be encrypted
-    :return: ciphertext
-    '''
     iv = os.urandom(16)
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
     encryptor = cipher.encryptor()
-    ciphertext = encryptor.update(plaintext.encode()) + encryptor.finalize()
+    ciphertext = encryptor.update(plaintext.encode('utf-8')) + encryptor.finalize()
     return iv + ciphertext
 
 def decrypt_message(key, ciphertext):
-    '''
-    Decrypt a message using AES in CBC mode
-    :param key: decryption key
-    :param ciphertext: ciphertext to be decrypted
-    :return: plaintext
-    '''
     iv = ciphertext[:16]
     ciphertext = ciphertext[16:]
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
     decryptor = cipher.decryptor()
     plaintext = decryptor.update(ciphertext) + decryptor.finalize()
-    return plaintext.decode()
+    return plaintext.decode('utf-8')
+
+def client_protocol(client, server_ip, server_port):
+    # Receive server's public key
+    server_public_bytes = client.recv()
+    server_public_key = serialization.load_pem_public_key(server_public_bytes.encode('utf-8'), backend=default_backend())
+
+    # Generate client's private key
+    parameters = dh.generate_parameters(generator=2, key_size=2048, backend=default_backend())
+    client_private_key = parameters.generate_private_key()
+    client_public_key = client_private_key.public_key()
+
+    # Send client's public key to server
+    client_public_bytes = client_public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+    client.send(client_public_bytes.decode('utf-8'))
+
+    # Generate shared secret
+    shared_key = client_private_key.exchange(server_public_key)
+    derived_key = derive_key(shared_key)
+
+    while True:
+        # Receive prompt from server
+        encrypted_rx_message = bytes.fromhex(client.recv())
+        rx_message = decrypt_message(derived_key, encrypted_rx_message)
+        rx_message_fields = rx_message.split(":")
+        rx_message_type = rx_message_fields[0]
+        rx_message_data = rx_message_fields[1]
+        logger.debug("Received: %s", rx_message)
+
+        if rx_message_type == "PROMPT":
+            # Get user input
+            user_response = input(rx_message_data + " ")
+
+            # Send response to server
+            tx_message = encrypt_message(derived_key, "RESPONSE:" + user_response)
+            client.send(tx_message.hex())
+        elif rx_message_type == "INFO":
+            print(rx_message_data)
+            break
+        else:
+            logger.error("Received unknown or incorrect message type: %s", rx_message_type)
+            return 1
+
+    return 0
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
@@ -90,24 +107,8 @@ if __name__ == '__main__':
     # Connect to the TCP server at the specified IP/port
     client.connect(args.ip, args.port)
 
-    # Receive server's public key
-    server_public_key = client.recv().encode()
-    server_public_key = serialization.load_pem_public_key(server_public_key, default_backend())
-
-    # Diffie-Hellman parameters setup
-    parameters = ec.generate_parameters(ec.SECP384R1(), default_backend())
-    private_key = parameters.generate_private_key()
-    public_key = private_key.public_key()
-
-    # Send client's public key to the server
-    client_public_bytes = public_key.public_bytes(serialization.Encoding.PEM, serialization.PublicFormat.SubjectPublicKeyInfo)
-    client.send(client_public_bytes.decode())
-
-    # Generate shared key
-    shared_key = generate_shared_key(private_key, server_public_key)
-
     # Communicate with the server
-    status = client_protocol(client, shared_key)
+    status = client_protocol(client, args.ip, args.port)
 
     # Close connection to server
     client.close()
